fd37a0dcfefc298183b8e0ba831aff4b54252f67
Ivan Gamanovich <igamanovich@elinext.com>
2022-10-15 18:10:27 +0300

    feat: add matreiaal

--- currency-converter-app/src/app/components/currency-date/currency-date.module.ts
+++ currency-converter-app/src/app/components/currency-date/currency-date.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CurrencyDateComponent} from "./currency-date.component";
-
+import {MatSliderModule} from "@angular/material/slider";
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import {CurrencyDateComponent} from "./currency-date.component";
     CurrencyDateComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule
   ],
   exports: [
     CurrencyDateComponent

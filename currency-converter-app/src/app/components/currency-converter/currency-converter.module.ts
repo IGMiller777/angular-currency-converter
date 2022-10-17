dcd5c6658ca5d5149f087070773344b669d57fd4
Ivan Gamanovich <igamanovich@elinext.com>
2022-10-17 19:24:59 +0300

    feat add new currency

--- currency-converter-app/src/app/components/currency-converter/currency-converter.module.ts
+++ currency-converter-app/src/app/components/currency-converter/currency-converter.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CurrencyConverterComponent} from "./currency-converter.component";
-
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -9,7 +18,17 @@ import {CurrencyConverterComponent} from "./currency-converter.component";
     CurrencyConverterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    NgxMatSelectSearchModule,
+    MatToolbarModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   exports: [
     CurrencyConverterComponent
--- currency-converter-app/src/app/services/flags-api.service.ts
+++ currency-converter-app/src/app/services/flags-api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {catchError, Observable, retry, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlagsApiService {
 
-  constructor() { }
+  public currencyNames: any = [];
+  protected readonly urlCurrencyNames: string = 'https://api.getgeoapi.com/v2/currency/list?api_key=491541064e413c945b459e8d015c07dc42d210c4&format=json';
+  constructor(private http: HttpClient) { }
+
+
+  public getCurrencyNames(): Observable<any> {
+    return this.http.get<any>(this.urlCurrencyNames).pipe(
+      retry(3),
+      catchError(error => {
+          console.log(error);
+          return of( null)
+        }
+      )
+    )
+  }
+
 }

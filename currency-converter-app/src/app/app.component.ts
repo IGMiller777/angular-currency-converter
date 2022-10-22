5fbb3dae5a3ceac05e03b59540bab6b580c626cd
Ivan Gamanovich <igamanovich@elinext.com>
2022-10-22 17:05:00 +0300

    feat: state before setInitial value

--- currency-converter-app/src/app/app.component.ts
+++ currency-converter-app/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'currency-converter-app';
+
+  public isReverse: boolean = true;
+
 }
--- currency-converter-app/src/app/components/currency-converter/currency-converter.component.ts
+++ currency-converter-app/src/app/components/currency-converter/currency-converter.component.ts
@@ -1,7 +1,8 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FlagsApiService} from "../../services/flags-api.service";
 import {CurrencyApiService} from "../../services/currency-api.service";
 import {HttpClient} from "@angular/common/http";
+import {ICountriesList, IUpdatedList} from "../../data/interfaces";
 import { MatSelect } from '@angular/material/select';
 import {
   BehaviorSubject,
@@ -21,22 +22,18 @@ import {Form, FormBuilder, FormControl, FormGroup, Validators} from "@angular/fo
   templateUrl: './currency-converter.component.html',
   styleUrls: ['./currency-converter.component.scss']
 })
-export class CurrencyConverterComponent implements OnInit, AfterViewInit, OnDestroy {
-  title = 'angular-mat-select-app';
-  selected: string | undefined;
+export class CurrencyConverterComponent implements OnInit, OnDestroy {
 
-  public currencyNames!:any;
-  public listOfCountries: any = [];
+  public isReverse: boolean = false;
+  public currencyNames!: ICountriesList;
+  public listOfCountries: IUpdatedList[] = [];
   protected _onDestroy = new Subject<void>();
-
   //Control 1
   public countryControl1: FormControl = new FormControl();
   public filterForCountryControl1: FormControl = new FormControl();
-  public filteredCountries1: ReplaySubject<any[]> = new ReplaySubject<any[]>(1)
+  public filteredCountries1: ReplaySubject<IUpdatedList[]> = new ReplaySubject<IUpdatedList[]>(1)
   @ViewChild('selectFlagOne') selectFlagOne!: MatSelect;
   @ViewChild('currencyInputOne', {static: true}) currencyInputOne!: ElementRef;
-
-  public currentValueOne: BehaviorSubject<any> = new BehaviorSubject<any>(0);
   public amountCurrency1: FormControl =  new FormControl("");
 
   //Control 2
@@ -45,35 +42,118 @@ export class CurrencyConverterComponent implements OnInit, AfterViewInit, OnDest
   public filteredCountries2: ReplaySubject<any[]> = new ReplaySubject<any[]>(1)
   @ViewChild('selectFlagTwo', {static: true}) selectFlagTwo!: MatSelect;
   @ViewChild('currencyInputTwo', {static: true}) currencyInputTwo!: ElementRef;
-
   public amountCurrency2: FormControl =  new FormControl("");
-
-  checkValue: FormControl = new FormControl('');
-
-
-  @ViewChild('currensySearchInput', {static: true}) currensySearchInput!: ElementRef
 
 
   constructor(private currencyFlagsService: FlagsApiService,
-              private currencyConvertService: CurrencyApiService,
-              private http: HttpClient) {
-
-  }
-
+              private currencyConvertService: CurrencyApiService) {}
 
   ngOnInit(): void {
     this.getNamesOfCurrencies();
     this.convertCurrencyFieldOne();
     this.convertCurrencyFieldTwo();
+  }
 
-  }
-  ngAfterViewInit() {
-    this.setInitialValue();
-  }
   ngOnDestroy() {
     this._onDestroy.next();
     this._onDestroy.complete();
   }
+
+  public getNamesOfCurrencies() {
+    return this.currencyFlagsService.getCurrencyNames().subscribe(res => {
+      this.currencyNames = res.currencies;
+      const keys = Object.keys(this.currencyNames)
+      keys.forEach((key: any, index:number) => {
+        this.listOfCountries.push({
+          id: index,
+          title: key,
+          fullName: this.currencyNames[key],
+          searchName: `${key} ${this.currencyNames[key]}`,
+          flag: key.slice(0,2).toLowerCase(),
+          flagConvert: key.slice(0,3).toUpperCase(),
+          image: 'https://countryflagsapi.com/svg/' + key.slice(0,2).toLowerCase()
+        });
+        return this.listOfCountries
+      })
+      this.setInitialValues();
+    })
+  }
+
+  public setInitialValues() {
+    this.countryControl1.setValue(this.listOfCountries);
+    console.log('@@==', this.countryControl1)
+    this.countryControl2.setValue(this.listOfCountries);
+
+    this.filteredCountries1.next(this.listOfCountries.slice());
+    this.filteredCountries2.next(this.listOfCountries.slice());
+
+    this.filterForCountryControl1.valueChanges
+      .pipe(
+        takeUntil(this._onDestroy),
+        debounceTime(200)
+      )
+      .subscribe(() => {
+        this.filterCountries1();
+      })
+
+    this.filterForCountryControl2.valueChanges
+      .pipe(
+        takeUntil(this._onDestroy),
+        debounceTime(200)
+      )
+      .subscribe(() => {
+        this.filterCountries2();
+      })
+  }
+
+  protected filterCountries1() {
+    if (!this.listOfCountries) {
+      return;
+    }
+    let search = this.filterForCountryControl1.value;
+    if (!search) {
+      this.filteredCountries1.next(this.listOfCountries.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    const newFilterCountre = this.listOfCountries.filter(
+      (item: any) => {
+        const result = item.searchName.toLowerCase().indexOf(search) > -1;
+        return result
+      })
+    this.filteredCountries1.next(newFilterCountre)
+  }
+  protected filterCountries2() {
+    if (!this.listOfCountries) {
+      return;
+    }
+    let search = this.filterForCountryControl2.value;
+    if (!search) {
+      this.filteredCountries2.next(this.listOfCountries.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    const newFilterCountre = this.listOfCountries.filter(
+      (item: any) => {
+        const result = item.searchName.toLowerCase().indexOf(search) > -1;
+        return result
+      })
+    this.filteredCountries2.next(newFilterCountre)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   public convertCurrencyFieldOne() {
@@ -128,25 +208,10 @@ export class CurrencyConverterComponent implements OnInit, AfterViewInit, OnDest
 
     }
   }
-  public setInitialValues() {
-    this.countryControl1.setValue(this.listOfCountries);
-    this.countryControl2.setValue(this.listOfCountries);
 
-    this.filteredCountries1.next(this.listOfCountries.slice());
-    this.filteredCountries2.next(this.listOfCountries.slice());
 
-    this.filterForCountryControl1.valueChanges
-      .pipe(takeUntil(this._onDestroy))
-      .subscribe(() => {
-        this.filterCountries1();
-      })
 
-    this.filterForCountryControl2.valueChanges
-      .pipe(takeUntil(this._onDestroy))
-      .subscribe(() => {
-        this.filterCountries2();
-      })
-  }
+
   public check() {
     const currencyFirst = this.selectFlagOne.ngControl.control?.value.title;
 
@@ -163,72 +228,4 @@ export class CurrencyConverterComponent implements OnInit, AfterViewInit, OnDest
   }
   public convertValue(value: any) {
   }
-  protected filterCountries1() {
-    if (!this.listOfCountries) {
-      return;
-    }
-    let search = this.filterForCountryControl1.value;
-    if (!search) {
-      this.filteredCountries1.next(this.listOfCountries.slice());
-      return;
-    } else {
-      search = search.toLowerCase();
-    }
-    const newFilterCountre = this.listOfCountries.filter(
-      (item: any) => {
-        const result = item.searchName.toLowerCase().indexOf(search) > -1;
-        return result
-      })
-    this.filteredCountries1.next(newFilterCountre)
-  }
-  protected filterCountries2() {
-    if (!this.listOfCountries) {
-      return;
-    }
-    let search = this.filterForCountryControl2.value;
-    if (!search) {
-      this.filteredCountries2.next(this.listOfCountries.slice());
-      return;
-    } else {
-      search = search.toLowerCase();
-    }
-    const newFilterCountre = this.listOfCountries.filter(
-      (item: any) => {
-        const result = item.searchName.toLowerCase().indexOf(search) > -1;
-        return result
-      })
-    this.filteredCountries2.next(newFilterCountre)
-  }
-  protected setInitialValue () {
-    // this.filteredCountries1.pipe(take(1), takeUntil(this._onDestroy)).subscribe(() => {
-    //   this.singleSelectFirst.compareWith= (a: any, b :any) => a && b && a.id === b.id
-    // })
-  }
-  public getNamesOfCurrencies() {
-    return this.currencyFlagsService.getCurrencyNames().subscribe(res => {
-      this.currencyNames = res.currencies;
-      const keys = Object.keys(this.currencyNames)
-      keys.forEach((key: any, index:number) => {
-        this.listOfCountries.push({
-          id: index,
-          title: key,
-          fullName: this.currencyNames[key],
-          searchName: `${key} ${this.currencyNames[key]}`,
-          flag: key.slice(0,2).toLowerCase(),
-          flagConvert: key.slice(0,3).toUpperCase(),
-          image: 'https://countryflagsapi.com/svg/' + key.slice(0,2).toLowerCase()
-        });
-        return this.listOfCountries
-      })
-      this.setInitialValues();
-    })
-
-  }
 }
-
-// required
-// Check Currensy - 1
-// Check Currensy - 2
-// AmountCurensy - 1
-//       ||
-// AmountCurrensy 2
